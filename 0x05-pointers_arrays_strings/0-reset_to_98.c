46819226d904d6dd2b78cd346eaa22a6026a525b
shimeket <shimeketa@gmail.com>
2022-12-18 23:33:44 -0800

    second update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * reset_to_98 - update the value of n to 98
  * @n: input
@@ -6,5 +7,5 @@
  */
 void reset_to_98(int *n)
 {
-	*n = 98
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * _putchar - writes character c to stdout
  * @c: char to be printed 
