4965f94dda99be9e3fd252e91ce890b792f86dfe
shimeket <shimeketa@gmail.com>
2022-12-28 15:51:07 -0800

    task 02

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _mul - multiplies two integers
+ * @a: first number
+ * @b: second number
+ * Return: returns product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
