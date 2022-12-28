1fd67e1134f3924bc9adc0c8a36947bfc7d3fe82
shimeket <shimeketa@gmail.com>
2022-12-28 15:35:36 -0800

    task 01

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ *@c: tested character
+ *Return: 1 f c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+		return (0);
+		return (0);
+}
