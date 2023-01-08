b77d8716b89bbabcf30347934042b626d5932c05
shimeket <shimeketa@gmail.com>
2023-01-08 04:59:18 -0800

    prime number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int square(int n, int val);
 int is_prime_number(int n);
+int check_prime(int n, int resp);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _putchar(char c);
