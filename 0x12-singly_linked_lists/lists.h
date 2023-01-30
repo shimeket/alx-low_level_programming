c4e9ed6ecc7c4fe03a97251e4b5be8a1edd2a3a0
shimeket <shimeketa@gmail.com>
2023-01-30 06:29:49 -0800

    task 00 final

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,16 +5,18 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string
- * @len: length of a string
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
