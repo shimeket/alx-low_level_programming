eb71bee012c3206aa1fd20d1d64c64b78fee0ed4
shimeket <shimeketa@gmail.com>
2023-02-06 09:28:13 -0800

    task 04 f

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -27,7 +27,8 @@ void check_elf(unsigned char *e_ident)
 
 	for (index = 0; index < 4; index++)
 	{
-	if (e_ident[index] != 127 && e_ident[index] != 'E' && e_ident[index] != 'L' && e_ident[index] != 'F')
+		if (e_ident[index] != 127 && e_ident[index] != 'E' &&
+		e_ident[index] != 'L' && e_ident[index] != 'F')
 		{
 			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 			exit(98);
@@ -44,7 +45,7 @@ void print_magic(unsigned char *e_ident)
 {
 	int index;
 
-	printf("  Magic:   ");
+	printf("Magic:");
 
 	for (index = 0; index < EI_NIDENT; index++)
 	{
@@ -63,7 +64,7 @@ void print_magic(unsigned char *e_ident)
  */
 void print_class(unsigned char *e_ident)
 {
-	printf("  Class:                             ");
+	printf("Class:");
 
 	switch (e_ident[EI_CLASS])
 	{
@@ -87,7 +88,7 @@ void print_class(unsigned char *e_ident)
  */
 void print_data(unsigned char *e_ident)
 {
-	printf("  Data:                              ");
+	printf("Data:");
 
 	switch (e_ident[EI_DATA])
 	{
@@ -111,12 +112,12 @@ void print_data(unsigned char *e_ident)
  */
 void print_version(unsigned char *e_ident)
 {
-	printf("  Version:                           %d", e_ident[EI_VERSION]);
+	printf("Version:%d", e_ident[EI_VERSION]);
 
 	switch (e_ident[EI_VERSION])
 	{
 	case EV_CURRENT:
-		printf(" (current)\n");
+		printf("(current)\n");
 		break;
 	default:
 		printf("\n");
@@ -130,7 +131,7 @@ void print_version(unsigned char *e_ident)
  */
 void print_osabi(unsigned char *e_ident)
 {
-	printf("  OS/ABI:                            ");
+	printf("OS/ABI:");
 
 	switch (e_ident[EI_OSABI])
 	{
@@ -179,7 +180,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
 
-	printf("  Type:                              ");
+	printf("Type:");
 
 	switch (e_type)
 	{
@@ -210,7 +211,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
  */
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
-	printf("  Entry point address:               ");
+	printf("Entry point address:");
 
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
