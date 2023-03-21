7ddad6f512adb13a666ef03eb6bf1fc94ac204ab
shimeket <shimeketa@gmail.com>
2023-03-20 19:30:49 -0700

    task 1 corrected

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-gcc -g -fPIC -Wall -Werror -Wextra -pedantic *.c
-gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -g -fPIC -Wall -Werror -Wextra -pedantic *.c -shared -o liball.so
 
