ccf2310ee2774a56a56ed553ed840b3664cbdf1f
shimeket <shimeketa@gmail.com>
2023-03-20 19:06:14 -0700

    task 0

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: print int
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+This is a project on 0x18. C - Dynamic libraries.
